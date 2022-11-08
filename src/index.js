661da03e24784ceb23cbd0656314c94f4beee2e6
VasylenkoDenys <vasylenkodenys92@gmail.com>
2022-11-09 01:42:43 +0200

    functional component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const product = {
+  productName: "Happy Dog",
+  productDescription: "Dry food for dogs",
+  price: 1500,
+  availability: 3,
+};
+
+function Section(props) {
+  const { productName, productDescription, price, availability} = product;
+  if (availability > 0){
+    return (
+      <>
+        <p>Product name is: {productName || 'Test Name'}</p>
+        <p>Product description: {productDescription || 'test description'}</p>
+        <p>Price: {price || 'test price'}</p>
+        <p>Product in stock</p>
+    </>
+  );
+}
+return (
+  <>
+    <p>Product name is: {productName}</p>
+    <p>Product description: {productDescription}</p>
+    <p>Price: {price}</p>
+    <p>Product out of stock</p>
+</>);
+}
+
+
+root.render(<Section/>);
